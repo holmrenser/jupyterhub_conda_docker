eda51bb4e86de32f101ad31977105a79159f110c
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2022-10-07 16:00:24 +0200

    conf

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,9 +1,12 @@
 c.JupyterHub.template_paths = ["/opt/jupyterhub/etc/jupyterhub/templates"]
 c.JupyterHub.admin_access = True
+
+c.NotebookApp.terminado_settings = {"shell_command": ["/bin/bash"]}
+c.Spawner.env_keep = ["SHELL", "PATH"]
+
 # CASE 4
 # Uncomment below if setting up Apache with a specific outside-facing URL
-# c.JupyterHub.bind_url = "https://0.0.0.0:8000/subdomain_name"
+c.JupyterHub.bind_url = "https://0.0.0.0:8000/biosb_metagenomics"
 c.LocalAuthenticator.create_system_users = True
-c.Authenticator.whitelist = {'user1', 'user2'}
-c.Authenticator.admin_users = {'useradmin'}
-
+c.Authenticator.whitelist = {"user1", "user2"}
+c.Authenticator.admin_users = {"useradmin"}
